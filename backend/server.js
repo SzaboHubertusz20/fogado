03f9a7da906f49d36b667bcd4b551ef3d19513fb
SzHubi <SzaHub608@hengersor.hu>
2025-10-07 13:20:14 +0200

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,59 @@
-
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+//könyvtárak importálása
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 5000;
-const mysql = require('mysql2');
+const mysql2 = require("mysql2");
  
+//középrétegek beállítása
 app.use(cors());
 app.use(express.json());
  
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: port
+//adatbázis kapcsolat létrehozása
+const db = mysql2.createConnection({
+    host: "localhost",    
+    user: "root",
+    password: "",
+    database: "fogado",
+    port: "3307"
 });
  
-app.get('/', (req, res) => {
-    res.send('API is running...');
+//A szerver működésének ellenőrzése
+app.get("/", (req, res) => {
+    res.json("Szerver működik");
 });
- 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+//szobák adatainak a lekérdezése
+app.get("/szobak/", (req, res) => {
+    const sql = "SELECT szobak.sznev, szobak.agy FROM szobak;";
+    db.query(sql, (err, result) => {
+        if (err) return res.json("Hiba a lekérdezés során");
+        return res.json(result);
+    });
 });
-
-
+//egy adott szoba adatainak a lekérdezése
+app.get("/szobak/:szoba", (req, res) => {
+    const sql = "SELECT szobak.sznev, szobak.agy FROM szobak where szobak.sznev = ?;";
+    db.query(sql,[req.params.szoba], (err, result) => {
+        if (err) return res.json("Hiba a lekérdezés során");
+        return res.json(result);
+    });
+});
+//vendégek adatainak a lekérdezése
+app.get("/vendej", (req, res) => {
+    const sql = "SELECT szobak.sznev, Count(foglalasok.vendeg) AS 'vendegek szama', SUM(foglalasok.tav-foglalasok.erk) AS 'foglalt éjszakák száma' FROM fogado.foglalasok join szobak ON szobak.szazon = foglalasok.szoba group by szobak.sznev ORDER BY Count(foglalasok.vendeg) ASC;";
+    db.query(sql, (err, result) => {
+        if (err) return res.json("Hiba a lekérdezés során");
+        return res.json(result);
+    });
+});
+//egy adott szobához tartozó foglalások lekérdezése
+app.get("/foglalt/:szoba", (req, res) => {
+    const sql = "select vendegek.vnev, foglalasok.erk, foglalasok.tav FROM fogado.foglalasok join vendegek ON vendegek.vsorsz = foglalasok.vendeg join szobak ON szobak.szazon = foglalasok.szoba where szobak.sznev = ? ORDER BY vnev asc;";
+    db.query(sql, [req.params.szoba], (err, result) => {
+        if (err) return res.json("Hiba a lekérdezés során");
+        return res.json(result);
+    });
+});
+//szerver indítása
+app.listen(3001, () => {
+    console.log("Szerver elindult a 3001-es porton");
+});
